49767ccd925e2612d58c9629b6f1187fc9cf5b76
piyush.sharma <piyush.sharma@T16F.a1technology.local>
2014-10-15 06:14:00 +0530

    Formatted code.

--- src/com/surkus/android/fragment/FSRSurkusGoerRegistrationFragment.java
+++ src/com/surkus/android/fragment/FSRSurkusGoerRegistrationFragment.java
@@ -5,12 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 
 import com.surkus.android.R;
-import com.surkus.android.model.CSRSurkusUser;
-import com.surkus.android.networking.CSRWebConstants;
-import com.surkus.android.utils.CSRConstants;
 
 public class FSRSurkusGoerRegistrationFragment extends Fragment {
 
@@ -22,35 +18,12 @@ public class FSRSurkusGoerRegistrationFragment extends Fragment {
 		mSurkusGoerRegistationParentView = inflater.inflate(
 				R.layout.fragment_user_registration, container, false);
 		
-		initializeUserDetails();
+		//initializeUserDetails();
 
 		return mSurkusGoerRegistationParentView;
 	}
 
 	void initializeUserDetails() {
-		Bundle userInformation = getArguments();
-		if (userInformation != null) {
-			CSRSurkusUser surkusUser= (CSRSurkusUser) userInformation.getSerializable(CSRWebConstants.SURKUS_USER);
-			String userName = surkusUser.getName();
-			String userEmailID = surkusUser.getEmailID();
-			String userGender = surkusUser.getGender();
-			String userDOB = surkusUser.getDateOfBirth();
-
-			if(userName != null)
-				((EditText)mSurkusGoerRegistationParentView.findViewById(R.id.name_edittext)).setText(userName);
-			
-
-			if(userEmailID != null)
-				((EditText)mSurkusGoerRegistationParentView.findViewById(R.id.email_edittext)).setText(userEmailID);
-			
-
-			if(userGender != null)
-				((EditText)mSurkusGoerRegistationParentView.findViewById(R.id.gender_edittext)).setText(userGender);
-			
-
-			if(userDOB != null)
-				((EditText)mSurkusGoerRegistationParentView.findViewById(R.id.dob_edittext)).setText(userDOB);
 		
-		}
 	}
 }
