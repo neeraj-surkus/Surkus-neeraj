e4bd6da333a41a35d326bdab2bcabd7a1637a45f
piyush.sharma <piyush.sharma@T16F.a1technology.local>
2014-10-15 06:21:12 +0530

    Added class to hold response returned from Surkus API.

---
+++ src/com/surkus/android/model/CSRSurkusApiResponse.java
@@ -0,0 +1,21 @@
+package com.surkus.android.model;
+
+public class CSRSurkusApiResponse {
+
+	int statusCode;
+	String responseData;
+	
+	public int getStatusCode() {
+		return statusCode;
+	}
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+	public String getResponseData() {
+		return responseData;
+	}
+	public void setResponseData(String responseData) {
+		this.responseData = responseData;
+	}
+	
+}
