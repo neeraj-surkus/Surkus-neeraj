ef4f581814beac61aaca670b8096990613b0de0f
piyush.sharma <piyush.sharma@T16F.a1technology.local>
2014-10-15 06:17:34 +0530

    Added web cosnstants.

--- src/com/surkus/android/networking/CSRWebConstants.java
+++ src/com/surkus/android/networking/CSRWebConstants.java
@@ -3,7 +3,6 @@ package com.surkus.android.networking;
 public class CSRWebConstants {
 	public static String SERVER_ERROR = "Server Error";
 	public static String NO_REPONSE_FROM_SERVER = "No response from server.";
-	public static String SKSTOKEN_KEY = "skstoken";
 	
 	public static String FB_ID = "fbid";
 	public static String FB = "fb";
@@ -15,4 +14,15 @@ public class CSRWebConstants {
 	
 	public static String SURKUS_USER = "surkus_user";
 	public static String SURKUS_USER_ERROR_MSG = "Not able to fetch Surkus User information.";
+	
+	// Surkus Token
+	public static String SURKUS_TOKEN_KEY = "ouath0";	
+	// Surkus user creation constants
+	public static String ZIPCODE = "zip";
+	
+	//Status codes	
+	public static int STATUS_CODE_200 = 200;
+	public static int STATUS_CODE_400 = 400;
+	public static int STATUS_CODE_401 = 401;
+	
 }
