346e8277f34fe103ef66f7be342139f76590de2f
piyush.sharma <piyush.sharma@T16F.a1technology.local>
2014-11-12 06:14:20 +0530

    Added surkus goger welcome messages.

--- src/com/surkus/android/adapter/CSRAboutSurkusAdapter.java
+++ src/com/surkus/android/adapter/CSRAboutSurkusAdapter.java
@@ -1,5 +1,6 @@
 package com.surkus.android.adapter;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -8,11 +9,15 @@ import com.surkus.android.R;
 import com.surkus.android.fragment.FSRHomeAboutSurkusFragment;
 
 public class CSRAboutSurkusAdapter extends FragmentPagerAdapter {
+	
+	int[] milAboutSurkusDrawables = {R.drawable.surkus_info_1,R.drawable.surkus_info_2,R.drawable.surkus_info_3,R.drawable.surkus_info_4,R.drawable.surkus_info_6};
+	String[] mWelcomeInfo; 
 
-	int[] milAboutSurkusDrawables ={R.drawable.login_1,R.drawable.login_2,R.drawable.login_3,R.drawable.login_4,R.drawable.login_5};
-
-    public CSRAboutSurkusAdapter(FragmentManager fragmentManager, int inNoOfAboutSurkusPages) {
+    public CSRAboutSurkusAdapter(Context  inContext,FragmentManager fragmentManager) {
         super(fragmentManager);       
+        
+        mWelcomeInfo = inContext.getResources().getStringArray(R.array.welcome_info);
+        
     }
 
     // Returns total number of pages
@@ -24,6 +29,6 @@ public class CSRAboutSurkusAdapter extends FragmentPagerAdapter {
     // Returns the fragment to display for that page
     @Override
     public Fragment getItem(int position) {
-    	 return FSRHomeAboutSurkusFragment.newInstance(milAboutSurkusDrawables[position]);
+    	 return FSRHomeAboutSurkusFragment.newInstance(milAboutSurkusDrawables[position],mWelcomeInfo[position],position);
     }
 }
